65dcaa02d687dd798f1558824adfb978a5fb021d
Juan Sebastian Alvarez Yaguara <114540723+Juanchoman64@users.noreply.github.com>
2022-10-04 18:12:30 -0500

    Add files via upload

---
+++ Java Basico/java_b_openboot_ejercicio_tema_1/src/com/juanchoman/Main.java
@@ -0,0 +1,26 @@
+package com.juanchoman;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int variableInt = 4;
+        long variableLong = 3000L;
+        double variableDouble = 3.5d;
+        float variableFloat = 3.14f;
+        boolean variableBool = true;
+        String variableString = "Nombre";
+        char variableChar = 'a';
+
+        System.out.println("El valor de la variable int es "+variableInt);
+        System.out.println("El valor de la variable long es "+variableLong);
+        System.out.println("El valor de la variable double es "+variableDouble);
+        System.out.println("El valor de la variable float es "+variableFloat);
+        System.out.println("El valor de la variable boolean es "+variableBool);
+        System.out.println("El valor de la variable String es "+variableString);
+        System.out.println("El valor de la variable char es "+variableChar);
+
+
+    }
+}
